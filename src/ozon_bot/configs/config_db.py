9a0fc5c83b4c59fe0a9e812092de841941216a1a
Shurik <sanya.kucheryavvyh@bk.ru>
2026-05-13 13:27:05 +0300

    add validation token bot

--- src/ozon_bot/configs/config_db.py
+++ src/ozon_bot/configs/config_db.py
@@ -1,4 +1,4 @@
-from pydantic import Field
+from pydantic import Field, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv
 
@@ -6,9 +6,12 @@ load_dotenv()
 
 class Settings(BaseSettings):
     USERNAME_PSQL: str  = Field(min_length=4)
-    PASSWORD_PSQL: str  = Field(min_length=4)
+    PASSWORD_PSQL: SecretStr  = Field(min_length=4)
     HOST: str = Field(min_length=5)
     PORT: int = Field(ge=0)
+    DATABASE: str = Field(min_length=4)
+
+    TOKEN_BOT: SecretStr
 
     model_config = SettingsConfigDict(
         env_file='.env',
@@ -16,4 +19,11 @@ class Settings(BaseSettings):
         extra='ignore'
     )
 
-settings = Settings().model_dump()
+    @property
+    def database_url(self) -> str:
+        password = self.PASSWORD_PSQL.get_secret_value()
+        return f"postgresql://{self.USERNAME_PSQL}:{password}@{self.HOST}:{self.PORT}/{self.DATABASE}"
+
+settings = Settings()
+
+
